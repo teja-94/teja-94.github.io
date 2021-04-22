0739c5dc038c2ada6a82c15e0e2390e04d7c568f
teja804 <tejateju804@gmail.com>
2021-04-22 17:06:27 +0530

    Updates

---
+++ precache-manifest.acc8bda10341502940a5d76488cc3e3a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b1b48dafbc1bc9dea158816339ed7c",
+    "url": "/dev-teja.github.io/index.html"
+  },
+  {
+    "revision": "541bf413792bf3a2a88c",
+    "url": "/dev-teja.github.io/static/css/3.6a352120.chunk.css"
+  },
+  {
+    "revision": "b06dad6ef496db6150d5",
+    "url": "/dev-teja.github.io/static/css/4.6e14a8dd.chunk.css"
+  },
+  {
+    "revision": "a14fe9157a70f46c255c",
+    "url": "/dev-teja.github.io/static/css/main.3cafb3e8.chunk.css"
+  },
+  {
+    "revision": "269456d37773fffeebf6",
+    "url": "/dev-teja.github.io/static/js/2.5cfdc868.chunk.js"
+  },
+  {
+    "revision": "d0d8190170b812713050bd2e6e947e19",
+    "url": "/dev-teja.github.io/static/js/2.5cfdc868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "541bf413792bf3a2a88c",
+    "url": "/dev-teja.github.io/static/js/3.b6081709.chunk.js"
+  },
+  {
+    "revision": "b06dad6ef496db6150d5",
+    "url": "/dev-teja.github.io/static/js/4.ed9b15c7.chunk.js"
+  },
+  {
+    "revision": "a14fe9157a70f46c255c",
+    "url": "/dev-teja.github.io/static/js/main.1e3c8c5c.chunk.js"
+  },
+  {
+    "revision": "a3d6bf6069f677bb44c1",
+    "url": "/dev-teja.github.io/static/js/runtime-main.2181fd15.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/dev-teja.github.io/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/dev-teja.github.io/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "0070ad0a1c844bc3f11fa8744348a0cd",
+    "url": "/dev-teja.github.io/static/media/b-cube.0070ad0a.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/dev-teja.github.io/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/dev-teja.github.io/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/dev-teja.github.io/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "c1cccf7b4d64e9ff0fc943a8c43ca349",
+    "url": "/dev-teja.github.io/static/media/dgLogo.c1cccf7b.png"
+  },
+  {
+    "revision": "8760422e8fcb4c07c15588eaf9775cf4",
+    "url": "/dev-teja.github.io/static/media/gemLogo.8760422e.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/dev-teja.github.io/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/dev-teja.github.io/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "92876ea114c1a54fe4be52726709b720",
+    "url": "/dev-teja.github.io/static/media/milletsLogo.92876ea1.png"
+  },
+  {
+    "revision": "006539b6b63dd31b1a8ee863ac11ee0e",
+    "url": "/dev-teja.github.io/static/media/ppLogo.006539b6.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/dev-teja.github.io/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "ca9b1053bbd3cc91dea9577b16e75963",
+    "url": "/dev-teja.github.io/static/media/rkLogo.ca9b1053.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/dev-teja.github.io/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/dev-teja.github.io/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
